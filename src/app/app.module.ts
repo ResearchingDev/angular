dd44f716fe263d4ff509f7261fff8bd010940072
Vignesh BUI0094 <amvignesh@brightuitechnologies.com>
2024-12-26 17:22:42 +0530

    Template

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// angular import
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+// project import
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  
+  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
